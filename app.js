1e28991120899e4cfedb3d2c09d6c351d75a963a
Carletos <160264790+Carletoss@users.noreply.github.com>
2025-03-05 10:12:39 -0300

    app.js

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
-  let campo = document.querySelector(tag)
-      campo.innerHTML = texto;
-      if ('speechSynthesis' in window) {
-        let utterance = new SpeechSynthesisUtterance(texto);
-        utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.2; 
-        window.speechSynthesis.speak(utterance); 
-    } else {
-        console.log("Web Speech API não suportada neste navegador.");
-    }
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 
+  if ('speechSynthesis' in window) {
+    window.speechSynthesis.cancel(); // Interrompe qualquer fala anterior
+    let utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR'; 
+    utterance.rate = 1.2; 
+    window.speechSynthesis.speak(utterance);
+  } else {
+    console.log("Web Speech API não suportada neste navegador.");
+  }
 }
+
 function exibirMensagemInicial() {
   exibirTextoNaTela ('h1', 'Jogo do número secreto');
       exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
